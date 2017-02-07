99bfc49f2e93128829da645ed980150e42a70404
Rbvader <robvader92@gmail.com>
2017-02-07 16:30:12 +0100

    test

---
+++ app/src/main/java/nl/stadsfeestenmiddelburg/bob/stadsfeesten/Voorkant.java
@@ -0,0 +1,27 @@
+package nl.stadsfeestenmiddelburg.bob.stadsfeesten;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Voorkant extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voorkant);
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
